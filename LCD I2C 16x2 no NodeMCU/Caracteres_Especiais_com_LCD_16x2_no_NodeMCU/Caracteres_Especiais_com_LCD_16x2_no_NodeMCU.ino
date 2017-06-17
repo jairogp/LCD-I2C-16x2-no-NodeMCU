abdf2840a2d2fc2ac5e2cb6a421ebd548a41c3a9
jairogp <jairogp@users.noreply.github.com>
2017-06-17 19:40:36 -0300

    Update Caracteres_Especiais_com_LCD_16x2_no_NodeMCU.ino

--- LCD I2C 16x2 no NodeMCU/Caracteres_Especiais_com_LCD_16x2_no_NodeMCU/Caracteres_Especiais_com_LCD_16x2_no_NodeMCU.ino
+++ LCD I2C 16x2 no NodeMCU/Caracteres_Especiais_com_LCD_16x2_no_NodeMCU/Caracteres_Especiais_com_LCD_16x2_no_NodeMCU.ino
@@ -10,45 +10,45 @@ LiquidCrystal_I2C lcd(0x3F, 16, 2); //FUNÇÃO DO TIPO "LiquidCrystal_I2C"
 // copiar para aqui o codigo fornecido pela aplicação de criar caracteres
 // caracter 1
 byte punho[8] = {
-B11111,
+B00000,
 B00000,
 B11111,
-B00000,
 B11111,
-B00000,
+B11111,
+B11111,
 B11111,
 B00000
 };
 // caracter 2
 byte mao[8] = {
-  B01100,
-  B10010,
-  B10010,
-  B01100,
-  B00000,
-  B00000,
-  B00000,
-  B00000
-  };
+  B10000,
+  B11000,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111
+};
 // caractere 3
 byte youtube[8] = {
   B11111,
-  B11000,
-  B10100,
-  B10010,
-  B10001,
-  B10000,
-  B10000,
+  B11111,
+  B01111,
+  B00111,
+  B00111,
+  B01111,
+  B11111,
   B11111
 };
  byte youtube2[8] = {
   B11111,
-  B00011,
-  B00101,
-  B01001,
-  B10001,
-  B00001,
-  B00001,
+  B11110,
+  B11110,
+  B11110,
+  B11110,
+  B11110,
+  B11110,
   B11111
 };
 void setup() 
@@ -75,9 +75,9 @@ lcd.write(2); //escreve o caracter
 lcd.setCursor(2, 0);
 lcd.print(" ****CURTA****");
 lcd.setCursor(1, 1); //aponta para coluna 0, linha 2
-lcd.write(4); //escreve o caracter
-lcd.setCursor(0, 1); //aponta para coluna 0, linha 2
 lcd.write(3); //escreve o caracter
+lcd.setCursor(0, 1); //aponta para coluna 0, linha 2
+lcd.write(4); //escreve o caracter
 lcd.setCursor(2,1);
 lcd.print(" -INSCREVA-SE- ");
  
